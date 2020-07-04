e4a48e78b14c3a6d330ec5637e7aba3ac69448d9
rafaelmendes1 <rarafafa3245@gmail.com>
2020-07-03 21:43:56 -0300

    Creating project and git repository

---
+++ src/db/DB.java
@@ -0,0 +1,67 @@
+package db;
+
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class DB {
+	
+	private static Connection conn = null;
+	
+	public static Connection getConnction() {
+		try {
+			if(conn == null) {
+				Properties props = loadProperties();
+				String url = props.getProperty("dburl");
+				conn = DriverManager.getConnection(url, props);
+			}
+		}catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		return conn;
+	}
+	
+	private static Properties loadProperties() {
+		try(FileInputStream fs = new FileInputStream("db.properties")){
+			Properties props = new Properties();
+			props.load(fs);
+			return props;
+		}catch(Exception e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+	
+	public static void closeConnection() {
+		try {
+			if(conn != null) {
+				conn.close();
+			}
+		}catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+	
+	public static void closeStatement(Statement st) {
+		while(st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
+	public static void closeResultSet(ResultSet rs) {
+		while(rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+}
